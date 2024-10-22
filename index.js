251502e34b346a36eba1a6caa68e22d6980c5b42
Baishnawi Agrawal <baishnawiagrawal08@gmail.com>
2024-10-22 12:11:44 +0545

    feat:"adding api to get all movies"

--- index.js
+++ index.js
@@ -16,23 +16,37 @@ app.use(express.json());
 
 initializeDatabase();
 
-const newMovie = {
-  title: "Stree",
-  director: "Amar Kaushik",
-  genre: "Horror",
-};
+// const newMovie = {
+//   title: "Stree",
+//   director: "Amar Kaushik",
+//   genre: "Horror",
+// };
 
-async function createMovie(newMovie) {
+// async function createMovie(newMovie) {
+//   try {
+//     const movie = new Movies1(newMovie);
+//     const saveMovie = await movie.save();
+//     console.log("New Movie data:", saveMovie);
+//   } catch (error) {
+//     throw error;
+//   }
+// }
+
+// createMovie(newMovie);
+
+app.get("/", (req, res) => {
+  res.send("Hello, Express");
+});
+
+app.get("/movies1", async (req, res) => {
   try {
-    const movie = new Movies1(newMovie);
-    const saveMovie = await movie.save();
-    console.log("New Movie data:", saveMovie);
+    const allmovies = await Movies1.find();
+    res.json(allmovies);
   } catch (error) {
-    throw error;
+    console.log(error);
+    res.status(500).json({ error: "Internal server error" });
   }
-}
-
-createMovie(newMovie);
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
